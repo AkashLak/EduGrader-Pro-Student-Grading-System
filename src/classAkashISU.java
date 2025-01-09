29a6661b7734648a2829f278829fb3051c9e157a
AkashLak <akash.lak2006@gmail.com>
2025-01-08 21:27:08 -0500

    Committing local changes before merge

---
+++ src/classAkashISU.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author akash
+ */
+public class classAkashISU 
+{
+    //Declaring array variables of type integers, with size 50, to store Student Roll No, Mathematics Marks, Computer Marks, Biology Marks, Chemistry Marks and StudentTotal Marks for all students
+    
+    int [ ] Arr_StudRollNo = new int [50];
+    int [ ] Arr_Subj1Marks = new int [50];
+    int [ ] Arr_Subj2Marks = new int [50];
+    int [ ] Arr_Subj3Marks = new int [50];
+    int [ ] Arr_Subj4Marks = new int [50];
+    int [ ] Arr_StudTotMarks = new int [50];
+    
+    int [ ] Arr_Sorted_Subj1Marks = new int [50];
+    int [ ] Arr_Sorted_Subj2Marks = new int [50];
+    int [ ] Arr_Sorted_Subj3Marks = new int [50];
+    int [ ] Arr_Sorted_Subj4Marks = new int [50];
+    double [ ] Arr_Double_StudAveMarks = new double [50];
+    double [ ] Arr_Sorted_StudAveMarks = new double [50];
+    int [ ] Arr_Sorted_IndexbyRollNo = new int [50];
+
+
+
+    //Declaring array variables of type String, with size 50, to store Student Names, Student Grades, StudentAverageMarks and StudentFinalReults for all students
+    String [ ] Arr_StudNames = new String [50];
+    String [ ] Arr_StudFirstNames = new String [50];
+    String [ ] Arr_StudLastNames = new String [50];
+    String [ ] Arr_StudAveMarks = new String [50];
+    String [ ] Arr_StudGrades = new String [50]; 
+    String [ ] Arr_StudResults = new String [50];
+    
+}
